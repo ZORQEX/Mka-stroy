6b54366643b3d985d5ac53d8d9715c0043c59fdd
Timq11 <timkst8@gmail.com>
2026-06-12 09:22:44 +0500

    feat: секция клиентов и партнёров с переводом на 6 языков

--- components/HomeClient.tsx
+++ components/HomeClient.tsx
@@ -4,6 +4,7 @@ import type { Locale } from '@/i18n.config'
 import type { Dictionary } from '@/lib/i18n'
 import { Hero } from '@/sections/Hero'
 import { Services } from '@/sections/Services'
+import { Partners } from '@/sections/Partners'
 import Projects from '@/sections/Projects'
 import { Clients } from '@/sections/Clients'
 import { Process } from '@/sections/Process'
@@ -21,6 +22,7 @@ export function HomeClient({ locale, dict }: HomeClientProps) {
     <>
       <Hero dict={dict} locale={locale} />
       <Services dict={dict} locale={locale} />
+      <Partners dict={dict} locale={locale} />
       <Projects preview dict={dict} />
       <Clients dict={dict} locale={locale} />
       <Process dict={dict} locale={locale} />
